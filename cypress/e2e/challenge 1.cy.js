06e0e13fe9652e9e3d19f3ee2ef5311b9fcdb3b4
Ostenfor <56930166+Ostenfor@users.noreply.github.com>
2023-08-06 00:14:30 -0400

    Update challenge 1.cy.js

--- cypress/e2e/challenge 1.cy.js
+++ cypress/e2e/challenge 1.cy.js
@@ -4,7 +4,7 @@
 //Criteria: When a user visits the amazon.com website in the logged out state and clicks on the Watchlist menu item they are redirected to a login/signup page.
 
 describe('Amazon', () => {
-    it('click on the Watchlist menu item they are redirected to a login/signup page.', () => {
+    it('Should click on the Watchlist menu item and be redirected to a login/signup page.', () => {
         cy.visit('https://amazon.com');
         cy.get('#nav-link-accountList').trigger('mouseover');
         cy.get('.nav-text').contains('Watchlist').should('be.visible').click();
