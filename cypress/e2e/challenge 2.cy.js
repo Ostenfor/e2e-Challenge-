ff1b4e0a22f2df85ff302173a7190d73eb9d02bc
Ostenfor <56930166+Ostenfor@users.noreply.github.com>
2023-11-21 21:36:34 -0400

    test
    test

--- cypress/e2e/challenge 2.cy.js
+++ cypress/e2e/challenge 2.cy.js
@@ -7,10 +7,23 @@
 describe('Amazon', () => {
     it('should change the currency settings', () => {
         cy.visit('https://amazon.com');
-        cy.get('.icp-nav-link-inner').trigger('mouseover');
-        cy.get('.icp-flyout-change').contains('Change').click();
-        cy.get(':nth-child(7) > .a-radio > label > .a-icon').should('exist').click();
-        cy.get('.a-button-input').click();
-        cy.get('.icp-nav-link-inner').contains('AR').should('exist');
+        
+        cy.get('.icp-nav-link-inner > .nav-line-2') .trigger('mouseover');
+        cy.wait(1)
+        cy.get('#nav-flyout-icp > .nav-template > :nth-child(14) > .nav-text > .icp-flyout-change').contains('Change').click();
+        cy.wait(1)
+        cy.get('.a-dropdown-prompt').click();
+        cy.wait(1)
+        cy.get('#icp-currency-dropdown_20').click();
+        cy.wait(1)
+        cy.get('.a-button-input').click()
+        cy.wait(1)
+        cy.get('[href="/gp/goldbox?ref_=nav_cs_gb"]').click()
+        cy.wait(1)
+        cy.get('#twotabsearchtextbox').type('firehd10')
+        cy.wait(1)
+        cy.get('#nav-search-submit-button').click()
+        cy.wait(1)
+        cy.get('.a-list-item > form > :nth-child(7)').contains('JPY')
     });   
 });
